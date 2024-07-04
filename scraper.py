b4ef35f8079b7233418728c042095fd2d73a35a0
Elijah Cox <elicox0@gmail.com>
2024-07-04 14:29:20 -0700

    update .gitignore and python version

--- scraper.py
+++ scraper.py
@@ -1,2 +1,14 @@
 import requests
-import selenium
+from selenium import webdriver
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    # options.add_argument("no-sandbox")
+    # options.add_experimental_option("exclueSwitches")
+
+    driver = webdriver.Chrome(options)
+    driver.get("http://automated.pythonanywhere.com")
+    return driver
