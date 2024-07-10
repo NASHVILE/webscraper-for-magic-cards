4ccf84c14d12f82e91ee727702c7f9f5eaccf5dd
Gideon Cox <gideoncox565@gmail.com>
2024-07-10 14:00:59 -0700

    add url to scraper

--- scraper.py
+++ scraper.py
@@ -6,9 +6,17 @@ def get_driver():
     options.add_argument("disable-infobars")
     options.add_argument("start-maximized")
     options.add_argument("disable-dev-shm-usage")
-    # options.add_argument("no-sandbox")
+    options.add_argument("no-sandbox")
     # options.add_experimental_option("exclueSwitches")
 
     driver = webdriver.Chrome(options)
-    driver.get("http://automated.pythonanywhere.com")
     return driver
+
+def scrape_cards(driver, url):
+    content = driver.get(url).content
+    print(content)
+
+if __name__ == "__main__":
+    url = "https://gatherer.wizards.com/Pages/Search/Default.aspx?action=advanced&output=spoiler&method=visual&sort=abc%20,abc+&format=[%22Commander%22]"
+    driver = get_driver() 
+    scrape_cards(driver, url)
